3840d8b96f54336fae4ee80f51c5963e4854a6e1
Matt <janse180@umn.edu>
2018-06-27 03:03:10 -0500

    Reimplement ceph-volume tool in order to get around --no-mon-config bug.

--- src/osd/start_osd.sh
+++ src/osd/start_osd.sh
@@ -13,8 +13,19 @@ function start_osd {
   vgscan --mknodes
 
   log "Activating on OSD device $OSD_ID"
-  ceph-volume lvm activate $OSD_ID $OSD_UUID --no-systemd
+  activate_osd
+  #ceph-volume lvm activate $OSD_ID $OSD_UUID --no-systemd
 
   log "Starting OSD daemon for OSD.$OSD_ID"
   ceph-osd -f -i "${OSD_ID}" --setuser ceph --setgroup disk
+}
+
+function activate_osd {
+  DEV_PATH=$(find /dev -name osd-block-$OSD_UUID)
+  OSD_PATH=/var/lib/ceph/osd/ceph-$OSD_ID
+  mkdir -p $OSD_PATH
+  ceph-bluestore-tool --no-mon-config prime-osd-dir --dev $DEV_PATH --path $OSD_PATH
+  ln -snf $DEV_PATH $OSD_PATH
+  chown -R ceph:ceph $(realpath $DEV_PATH)
+  chown -R ceph:ceph $OSD_PATH
 }
