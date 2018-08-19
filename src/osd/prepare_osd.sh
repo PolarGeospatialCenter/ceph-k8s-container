29dcc6290e6b57412523df14a2eae1a9ac4e906a
Andrew Zenk <azenk@umn.edu>
2018-08-19 18:38:12 -0500

    Update prepare and start to use non-lvm osds

--- src/osd/prepare_osd.sh
+++ src/osd/prepare_osd.sh
@@ -2,28 +2,21 @@
 set -e
 
 function prepare_osd {
-  #Required Vars
-  : "${OSD_DEVICE?}"
-  : "${OSD_ZAP?}"
+  OSD_DEVICE=/dev/osd
+  # Exit if disk not overprovisioned
+  hdparmOutput=$(hdparm -N $OSD_DEVICE 2>/dev/null || echo -n "")
+  printf "$hdparmOutput" | gawk '{if (match($0, /([0-9]+)\/([0-9]+), HPA/, arr)){if (arr[1] == arr[2]){print "Disk not overprovisioned"; exit 1} }}' || exit 1
 
-  log "Editing lvm.conf..."
-  sed -i 's/udev_sync = 1/udev_sync = 0/g; s/udev_rules = 1/udev_rules = 0/' /etc/lvm/lvm.conf
+  UUID=$(uuidgen)
+  OSD_SECRET=$(ceph-authtool --gen-print-key)
+  OSD_ID=$(echo "{\"cephx_secret\": \"$OSD_SECRET\"}" | \
+      ceph osd new $UUID -i - -n client.bootstrap-osd -k /var/lib/ceph/bootstrap-osd/ceph.keyring)
 
-  find /sys/block/sd* |xargs -n 1 udevadm test > /dev/null 2>&1
-  OSD_DEVICE=$(realpath $OSD_DEVICE)
+  mkdir /ceph-osd
+  chown ceph:ceph /ceph-osd
 
-  if [ ! -f "/var/lib/ceph/bootstrap-osd/ceph.keyring" ]; then
-    log "Error: Ceph keyring does not exist."
-    exit 1
-  fi
-
-  if [ "$OSD_ZAP" == "true" ]; then
-    log "Zapping OSD device $OSD_DEVICE"
-    ceph-volume lvm zap $OSD_DEVICE --destroy
-  fi
-
-  log "Starting prepare on OSD device $OSD_DEVICE"
-  ceph-volume lvm prepare --bluestore --data $OSD_DEVICE
-
-  exit 0
+  ln -s /dev/osd /ceph-osd/block
+  ceph-authtool --create-keyring /ceph-osd/keyring --name osd.$ID --add-key $OSD_SECRET
+  ceph-osd --cluster ceph --osd-objectstore bluestore --mkfs -i $ID --osd-data /ceph-osd/ --osd-uuid $UUID --keyring /ceph-osd/keyring
+  chown -R ceph:ceph /ceph-osd/
 }
