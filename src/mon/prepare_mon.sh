b1900af6e21654047d26cbb4888121b31b9386c7
Matt Jansen <janse180@umn.edu>
2019-01-08 16:54:28 -0600

    Refactor to use cluster k8s object to get required parameters.

--- src/mon/prepare_mon.sh
+++ src/mon/prepare_mon.sh
@@ -3,8 +3,12 @@ set -e
 
 function prepare_mon {
   #Required Vars
-  : "${FSID?}"
-  : "${MON_NAME?}"
+  : "${CLUSTER?}"
+  : "${CLUSTER_NAMESPACE?}"
+  : "${PV_LABEL_SELECTOR?}"
+
+  FSID=$(kubectl get cephcluster $CLUSTER -n $CLUSTER_NAMESPACE -o template --template="{{.spec.fsid}}")
+  MON_ID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 6 | head -n 1)
 
   touch /etc/ceph/ceph.conf
 
@@ -32,10 +36,24 @@ function prepare_mon {
 
   # Prepare the monitor daemon's directory with the map and keyring
   #ceph-mon --setuser ceph --setgroup ceph --cluster "${CLUSTER}" --mkfs -i "${MON_NAME}" --inject-monmap "$MON_MAP" --keyring "$MON_KEYRING" --mon-data "$MON_DATA_DIR"
-  ceph-mon --setuser ceph --setgroup ceph --cluster "${CLUSTER}" --mkfs -i "${MON_NAME}" --fsid $FSID --keyring $MON_KEYRING --mon-data "$MON_DATA_DIR" 
+  ceph-mon --setuser ceph --setgroup ceph --cluster "${CLUSTER}" --mkfs -i "${MON_ID}" --fsid $FSID --keyring $MON_KEYRING --mon-data "$MON_DATA_DIR"
 
   # dir=$(ls -lah $MON_DATA_DIR)
   # log $dir
+
+  cat << EOF > /tmp/mon.yaml
+kind: CephMon
+version: ceph.k8s.pgc.umn.edu/v1alpha1
+metadata:
+  name: $CLUSTER-mon.$MON_ID
+  namespace: $CLUSTER_NAMESPACE
+spec:
+  clusterName: $CLUSTER
+  ID: $MON_ID
+  PvSelector: $PV_LABEL_SELECTOR
+EOF
+
+  kubectl apply -f /tmp/mon.yaml
 
   exit 0
 }
