c2af83c1c64292fd816526e163f8fc2d0c3288d4
Andrew Zenk <azenk@umn.edu>
2019-01-14 15:39:16 -0600

    Update health check to use MON_ID instead of MON_NAME

--- bin/mon_health.sh
+++ bin/mon_health.sh
@@ -2,10 +2,10 @@
 set -e
 
 #Require mon name
-: "${MON_NAME?}"
+: "${MON_ID?}"
 
 #Connect to admin socket and get state
-state=$(timeout 5 ceph daemon mon.$MON_NAME mon_status |jq -r .state)
+state=$(timeout 5 ceph daemon mon.$MON_ID mon_status |jq -r .state)
 
 #Check if state is valid.
 if [ "$state" = "leader" ] || [ "$state" = "peon" ]; then
