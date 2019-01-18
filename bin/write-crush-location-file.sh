2e4b21104c9319c1cdca2c8b2f56f2a8cb37664e
Matt Jansen <janse180@umn.edu>
2019-01-17 19:07:27 -0600

    Update crush location

--- bin/write-crush-location-file.sh
+++ bin/write-crush-location-file.sh
@@ -8,4 +8,4 @@ ROOM=$(echo ${NODE_JSON} | jq -r .metadata.labels.room)
 RACK=$(echo ${NODE_JSON} | jq -r .metadata.labels.rack)
 NODE=$(echo -n ${NODE_NAME} | cut -f 1 -d '.')
 
-echo "root=default datacenter=${BUILDING}-${ROOM} rack=${RACK} node=${NODE}" > /ceph/crush-location
+echo "root=default datacenter=${BUILDING} room=${ROOM} rack=${RACK} host=${NODE}" > /ceph/crush-location
