2b662ad204b05a6137c60cf0cbb6edcf4aca500a
Matt Jansen <janse180@umn.edu>
2019-01-24 17:49:49 -0600

    Update CLI opts

--- src/keyrings/sync_keyrings.sh
+++ src/keyrings/sync_keyrings.sh
@@ -6,17 +6,17 @@ function sync_keyrings {
   : "${CLUSTER?}"
 
   ceph-conf --lookup mon_host
-  keyring=$(ceph-conf --lookup keyring)
+  keyring=$(ceph-conf "${CLI_OPTS[@]}" --lookup keyring)
 
   if [[ ! -e $keyring ]]; then
     log "Keyring $keyring does not exist"
   fi
 
-  ping -c 1 $(ceph-conf --lookup mon_host) || true
+  ping -c 1 $(ceph-conf "${CLI_OPTS[@]}" --lookup mon_host) || true
 
   ceph mon dump
 
-  quorum=$(timeout 5 ceph mon dump 2> /dev/null > /dev/null)$? || true
+  quorum=$(timeout 5 ceph "${CLI_OPTS[@]}" mon dump 2> /dev/null > /dev/null)$? || true
   log "Check for quorum returned $quorum"
   # If quorum add monitor
   if [[ $quorum -ne 0 ]] ; then
@@ -24,12 +24,12 @@ function sync_keyrings {
     exit 1
   fi
 
-  keyrings=$(ceph auth list 2> /dev/null |grep client.)
+  keyrings=$(ceph "${CLI_OPTS[@]}" auth list 2> /dev/null |grep client.)
 
   log "Found keyrings $keyrings"
 
   for entity in $keyrings; do
-    ceph auth get $entity 2>/dev/null > keyring
+    ceph "${CLI_OPTS[@]}" auth get $entity 2>/dev/null > keyring
     secretName="ceph-$CLUSTER-$entity-keyring"
 
     log "Syncing keyring $entity to kuberentes with name $secretName"
