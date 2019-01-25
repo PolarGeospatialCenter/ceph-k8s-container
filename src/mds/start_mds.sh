2e28ff7c9a87c1f7c0afa53ba15ef2474db59dbb
Matt Jansen <janse180@umn.edu>
2019-01-24 21:44:25 -0600

    Use generic ENV var for daemon IDs

--- src/mds/start_mds.sh
+++ src/mds/start_mds.sh
@@ -2,6 +2,8 @@
 set -e
 
 function start_mds {
+  : "${DAEMON_ID?}"
+
   check_config
 
   # Check to see if our MDS has been initialized
@@ -10,7 +12,7 @@ function start_mds {
     #check_admin_key
     # Create ceph-mds key
     ceph "${CLI_OPTS[@]}" auth --keyring $MDS_BOOTSTRAP_KEYRING --name client.bootstrap-mds \
-      get-or-create mds."$MDS_NAME" osd "allow rwx" mds "allow" mon "allow profile mds" -o "$MDS_KEYRING"
+      get-or-create mds."$DAEMON_ID" osd "allow rwx" mds "allow" mon "allow profile mds" -o "$MDS_KEYRING"
     chown "${CHOWN_OPT[@]}" ceph. "$MDS_KEYRING"
     chmod 600 "$MDS_KEYRING"
   fi
@@ -18,5 +20,5 @@ function start_mds {
   log "Starting Ceph Mgr"
   # start ceph-mds
   #while true; do sleep 2; done
-  exec /usr/bin/ceph-mds "${DAEMON_OPTS[@]}" --keyring $MDS_KEYRING -i "$MDS_NAME" -d
+  exec /usr/bin/ceph-mds "${DAEMON_OPTS[@]}" --keyring $MDS_KEYRING -i "$DAEMON_ID" -d
 }
--- src/mgr/start_mgr.sh
+++ src/mgr/start_mgr.sh
@@ -2,7 +2,7 @@
 set -e
 
 function start_mgr {
-  : "${MGR_ID?}"
+  : "${DAEMON_ID?}"
 
   check_config
 
@@ -12,7 +12,7 @@ function start_mgr {
     #check_admin_key
     # Create ceph-mgr key
     ceph "${CLI_OPTS[@]}" auth --keyring $MGR_BOOTSTRAP_KEYRING --name client.bootstrap-mgr \
-      get-or-create mgr."$MGR_ID" mon 'allow profile mgr' osd 'allow *' mds 'allow *' -o "$MGR_KEYRING"
+      get-or-create mgr."$DAEMON_ID" mon 'allow profile mgr' osd 'allow *' mds 'allow *' -o "$MGR_KEYRING"
     chown "${CHOWN_OPT[@]}" ceph. "$MGR_KEYRING"
     chmod 600 "$MGR_KEYRING"
   fi
@@ -20,5 +20,5 @@ function start_mgr {
   log "Starting Ceph Mgr"
   # start ceph-mgr
   #while true; do sleep 2; done
-  exec /usr/bin/ceph-mgr "${DAEMON_OPTS[@]}" --keyring $MGR_KEYRING -i "$MGR_ID"
+  exec /usr/bin/ceph-mgr "${DAEMON_OPTS[@]}" --keyring $MGR_KEYRING -i "$DAEMON_ID"
 }
