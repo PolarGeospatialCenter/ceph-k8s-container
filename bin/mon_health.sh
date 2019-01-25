9d772c29b83a6bb9fbd1af4349ae793ad3e08481
Matt Jansen <janse180@umn.edu>
2019-01-24 18:03:46 -0600

    Add cluster

--- bin/mon_health.sh
+++ bin/mon_health.sh
@@ -3,9 +3,10 @@ set -e
 
 #Require mon name
 : "${MON_ID?}"
+: "${CLUSTER?}"
 
 #Connect to admin socket and get state
-state=$(timeout 5 ceph daemon mon.$MON_ID mon_status |jq -r .state)
+state=$(timeout 5 ceph --cluster $CLUSTER daemon mon.$MON_ID mon_status |jq -r .state)
 
 #Check if state is valid.
 if [ "$state" = "leader" ] || [ "$state" = "peon" ]; then
